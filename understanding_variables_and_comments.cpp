de40709c979d8e131f21601ef752e777ce007241
lakshyatekriwal66 <lakshyatekriwal66@gmail.com>
2026-05-15 13:53:32 +0530

    Learning Variables and Comments

--- understanding_variables_and_comments.cpp
+++ understanding_variables_and_comments.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
 
+//this is comments
+/*Learning variables and comments*/
 
 int main(){
+
     int sum = 9;
     cout << "Hello world"<< sum;
     return 0;
