6d9c5a633c700ff2c97c017a062362b9dfa331c3
lakshyatekriwal66 <lakshyatekriwal66@gmail.com>
2026-05-17 19:07:39 +0530

    Leap year
    Made a program which will conclude that the given year is a leap year or not
    Got stuck in the conditions of leap year
    but figured it out at the end

---
+++ Leap_Year.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std;
+int main(){
+
+    int year;
+    cout << "Enter the year: ";
+    cin >> year;
+
+    if((year % 4 == 0) && (year % 100 !=0)){
+        cout << year << " is a Leap year." << endl;
+    }
+     else if ((year % 100 == 0) && (year % 400 == 0)){
+        cout << year << " is a Leap year." << endl;
+     }
+     else{
+        cout << year << " is not a Leap year." << endl;
+     }
+    return 0;
+}
