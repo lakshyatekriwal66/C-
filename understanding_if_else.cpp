61d692d43dd4f83cbf0db8e692049c32e63e2532
lakshyatekriwal66 <lakshyatekriwal66@gmail.com>
2026-05-17 18:24:41 +0530

    Understanding if else
    Learned how to use if else and switch-case and made a program to distinguish larger number between three number and using switch-case made a program to distinguish positive or negative number

---
+++ understanding_if_else.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int a, b, c;
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+    cout << "Enter a third number: ";
+    cin >> c;
+
+    if  (a >= b && a >= c) {
+        cout << "The Largest number is: " << a << endl;
+    }
+    else if (b >= a && b >= c){ 
+        cout << "The Largest number is: " << b << endl;
+    }
+    else {
+        cout << "The Largest number is: " << c << endl;
+    }
+    int x;
+    cout << "Enter a number: ";
+    cin >> x; 
+
+    switch (x>0)
+    {
+    case 1:
+        cout << "The number is positive." << endl;
+        break;
+    
+    default:
+        cout << "The number is not positive." << endl;
+        break;
+    }
+    return 0;
+}
